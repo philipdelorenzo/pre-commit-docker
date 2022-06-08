8586af192b6f85451dfd5c85a5dd31b2ae06b162
Philip De Lorenzo <philip.delorenzo@gmail.com>
2022-06-08 12:10:11 -0700

    Add changes so that git can check the code

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,6 +7,9 @@ export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
 # shellcheck disable=SC1090
 . ~/.asdf/asdf.sh
 
+# Add this safe directory so we can check the code
+git config --global --add safe.directory /src
+
 cd /src || exit 1 && pre-commit run --all-files \
   && echo "Checking Markdown code..." \
   && markdownlint . \
