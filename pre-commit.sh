3b0f01abdd2abc54b9b70a03adf788242fa44011
Philip De Lorenzo <35271545+philipdelorenzo@users.noreply.github.com>
2022-06-28 09:25:57 -0700

    Add change to prettier method (#7)

--- pre-commit.sh
+++ pre-commit.sh
@@ -11,9 +11,9 @@ export PATH=/home/linter:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:$PATH
 git config --global --add safe.directory /src
 
 cd /src || exit 1 && pre-commit run --all-files \
-  && echo "[ADDON] - Checking HTML files (prettier)..." \
-  && npx prettier --write . \
   && echo "[ADDON] - Checking CircleCi Config (circleci)..." \
-  && if [ -d "/src/.circleci" ]; then /home/linter/circleci config validate /src/.circleci/config.yml; else echo "No CircleCi config found...continuing."; fi
+  && if [ -d "/src/.circleci" ]; then /home/linter/circleci config validate /src/.circleci/config.yml; else echo "No CircleCi config found...continuing."; fi \
+  && echo "[ADDON] - Checking HTML files (prettier)..." \
+  && npx prettier --embedded-language-formatting=off --check .
   #&& echo "Checking Ruby code..." \
   #&& rufo . \
